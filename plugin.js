89acc259e04b06b42d8e734f21d9adcc2edca8fe
Pascale Beier <mail@pascalebeier.de>
2017-04-05 15:10:23 +0200

    Update plugin.js

--- plugin.js
+++ plugin.js
@@ -3,13 +3,13 @@
  * Created by ALL-INKL.COM - Neue Medien M�nnich - 04. Feb 2014
  * Licensed under the terms of GPL, LGPL and MPL licenses.
  */
-CKEDITOR.plugins.add("pb-base64image-for-ckedtor", {
+CKEDITOR.plugins.add("pb-base64image-for-ckeditor", {
 	lang 	: 	["af","ar","bg","bn","bs","ca","cs","cy","da","de","de-ch","el","en","en-au","en-ca","en-gb","eo","es","et","eu","fa","fi","fo","fr","fr-ca","gl","gu","he","hi","hr","hu","id","is","it","ja","ka","km","ko","ku","lt","lv","mk","mn","ms","nb","nl","no","pl","pt","pt-br","ro","ru","si","sk","sl","sq","sr","sr-latn","sv","th","tr","ug","uk","vi","zh","zh-cn"],
 	requires: 	"dialog",
 	icons	:	"base64image",
 	hidpi	:	true,
     init	: 	function(editor){
-					var pluginName = 'pb-base64image-for-ckedtorDialog';
+					var pluginName = 'base64imageDialog';
 					
 					editor.ui.addButton("base64image", {
 						label: editor.lang.common.image,
@@ -39,9 +39,9 @@ CKEDITOR.plugins.add("pb-base64image-for-ckedtor", {
 						editor.addMenuGroup("base64imageGroup");
 						editor.addMenuItem("base64imageItem", {
 							label: editor.lang.common.image,
-							icon: this.path+"icons/pb-base64image-for-ckedtor.png",
+							icon: this.path+"icons/base64image.png",
 							command: pluginName,
-							group: "pb-base64image-for-ckedtorGroup"
+							group: "base64imageGroup"
 						});
 					}
 					if(editor.contextMenu) {
